e0bf5d6bf242bc2535ccee6c3bd5fff832e8c332
João Santos <joao.santos@pdmfc.com>
2020-02-22 22:54:32 +0100

    add brew.sh

---
+++ brew.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Install Brew Packages
+
+
+# Install MacOS Applications
+brew cask install google-chrome
+brew cask install firefox
+brew cask install spotify
+brew cask install discord
+brew cask install skype
+brew cask install vlc
+brew cask install visual-studio-code
