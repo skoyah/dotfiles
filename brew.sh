f171778ffceb0051b7e07c82f61d8fc917826f89
João Santos <joao.santos@pdmfc.com>
2020-02-24 09:15:30 +0100

    add brew dependencies

--- brew.sh
+++ brew.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
-# Install Brew Packages
+brew update
 
+# Install Brew Packages
+brew install wget
+brew install composer
+brew install zsh
+brew install php
+brew install mysql
+brew install node
+brew install npm
 
 # Install MacOS Applications
 brew cask install google-chrome
@@ -11,3 +19,15 @@ brew cask install discord
 brew cask install skype
 brew cask install vlc
 brew cask install visual-studio-code
+brew cask install postman
+brew cask install stremio
+brew cask install iterm2
+brew cask install tableplus
+brew cask install phpstorm
+
+# Fonts
+brew tap homebrew/cask-fonts
+brew cask install font-jetbrains-mono
+brew cask install font-lato
+brew cask install font-open-sans
+brew cask install font-roboto
